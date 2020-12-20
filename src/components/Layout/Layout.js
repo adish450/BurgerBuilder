ad377e76d5f95e9e0e98ac681e767fcf6c2b511a
Adish <adishirfan17@gmail.com>
2020-12-21 00:31:19 +0530

    Added Project

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Auxiliary/Auxiliary';
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
+
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false })
+    }
+
+    toggleShowSideDrawer = () => {
+        //more preferred way to manipulate state when using prev state
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer }
+        })
+    }
+    render() {
+        return (
+            <Aux>
+                <Toolbar clicked={this.toggleShowSideDrawer} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>{this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+export default Layout;
