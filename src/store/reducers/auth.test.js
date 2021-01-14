10376f2d05b9c3327607c096e8be437c35fddc63
Adish <adishirfan17@gmail.com>
2021-01-15 00:25:27 +0530

    testing reducers

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,32 @@
+import reducer from './auth';
+import * as actionTypes from '../actions/actionTypes';
+
+describe('auth reducer', () => {
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {})).toEqual({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false
+        })
+    });
+
+    it('should store the token upon login', () => {
+        expect(reducer({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false
+        }, {
+            type: actionTypes.AUTH_SUCCESS,
+            idToken: 'idToken',
+            userId: 'localId'
+        })).toEqual({
+            token: 'idToken',
+            userId: 'localId',
+            error: null,
+            loading: false
+        })
+    })
+
+})
